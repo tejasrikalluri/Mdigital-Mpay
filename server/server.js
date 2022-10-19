89271fa4ac84ea9e02fa5fff8079b4a26f768cd3
tejasrikalluri <tkalluri@artissol.com>
2022-10-20 00:03:02 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+exports = {
+
+  // args is a JSON block containing the payload information.
+  // args['iparam'] will contain the installation parameter values.
+  onTicketCreateHandler: function(args) {
+    console.log('Hello ' + args['data']['requester']['name']);
+  }
+
+};
