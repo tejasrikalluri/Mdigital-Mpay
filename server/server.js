f5b7df4824cbc33b3fc5ea822e3bb720b7d55c1b
tejasrikalluri <tkalluri@artissol.com>
2023-02-25 22:28:45 +0530

    modified ticket create in mondia pay & updated hyperlink css in iparams

--- server/server.js
+++ server/server.js
@@ -2,52 +2,36 @@ exports = {
   onTicketCreateHandler: function (args) {
     console.log("%%%%%%%%%%%%%%%%%%%%")
     const { id } = args.data.ticket;
-    fetchTicketData(id, args.iparams);
+    const { email } = args.data.requester;
+    fetchTicketData(id, args.iparams, email);
   }
 };
-let fetchTicketData = async (id, iparams) => {
-  let err, reply;
-  [err, reply] = await to($request.invokeTemplate("fetchTicketData", { "context": { id } }));
-  console.log(reply);
-  if (err) {
-    console.log("@FETCH TICKET DATA")
-    console.log(err);
-  }
+let fetchTicketData = async (id, iparams, email) => {
   try {
-    let ticketResp = JSON.parse(reply.response).ticket;
-    const custom_fields = Object.keys(ticketResp.custom_fields).filter((key) => key.includes(iparams.checkboxes)).reduce((obj, key) => {
-      return Object.assign(obj, {
-        [key]: ticketResp.custom_fields[key]
-      });
-    }, {});
-    if (custom_fields[iparams.checkboxes]) formBodyForTicketCreation(ticketResp);
+    let data = await $request.invokeTemplate("fetchTicketData", { context: { id } });
+    let ticketResp = JSON.parse(data.response).ticket;
+    console.log(ticketResp.custom_fields)
+    if (Object.keys(ticketResp.custom_fields).includes(iparams.checkboxes) && ticketResp.custom_fields[iparams.checkboxes]) {
+      formBodyForTicketCreation(ticketResp, email);
+    }
   } catch (error) {
-    console.log("@CATCH FETCH TICKET DATA")
-    console.log(error)
+    console.log("@FETCH TICKET DATA")
+    console.error(error);
   }
 };
-function to(promise, improved) {
-  return promise
-    .then((data) => [null, data])
-    .catch((err) => {
-      if (improved) {
-        Object.assign(err, improved);
-      }
-      return [err];
-    });
-}
-let formBodyForTicketCreation = async (ticketResp) => {
+let formBodyForTicketCreation = async (ticketResp, email) => {
   let { subject, description, priority, id } = ticketResp;
   console.log(subject, description, priority, id)
   let body = {
-    subject, description, priority, status: 2
+    subject, description, priority, status: 2, email, mpay_priority: 'Medium'
   };
-  console.log(body)
-  let err, reply;
-  [err, reply] = await to($request.invokeTemplate("createTicket", { body: JSON.stringify(body) }));
-  console.log(reply);
-  if (err) {
+  console.log(JSON.stringify(body))
+  try {
+    let data = await $request.invokeTemplate("createTicket", { body: JSON.stringify(body) });
+    console.log("CCCCCCCCCCCCCCCCCCCCCc")
+    console.log(data.response)
+  } catch (error) {
     console.log("@TICKET CREATION IN MP")
-    console.log(err)
+    console.error(error)
   }
 };
